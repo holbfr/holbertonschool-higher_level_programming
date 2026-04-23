1ea05b5f7d5651e71593e14063360767cc1048cd
holbfr <houssemeddine.benkhalifa@holbertonschool-laval.fr>
2026-04-23 17:07:48 +0200

    task 1 done

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Simple module for task validation
+"""
+import pickle
+
+
+class CustomObject():
+    """CustomObject class definition"""
+
+    def __init__(self, name, age, is_student):
+        """CustomObject class constructor"""
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """Display instance information to stdout"""
+        print("Name: {}\nAge: {}\nIs Student: {}".format(
+            self.name, self.age, self.is_student))
+
+    def serialize(self, filename):
+        """Serializes the instance and saves it to file"""
+        with open(filename, 'wb') as file:
+            pickle.dump(self, file)
+
+    @classmethod
+    def deserialize(cls, filename):
+        """Deserializes data from file (filename) into a python object"""
+        with open(filename, 'rb') as file:
+            loaded_data = pickle.load(file)
+        return loaded_data
